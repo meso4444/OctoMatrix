286f5bb5fc5cc4f4338e91b34e507b7ef3db78f7
meso4444 <meso4444@users.noreply.github.com>
2026-05-21 17:41:01 +0800

    fix: correct agent_dir path in start script test message block to properly check .rotation_flag

--- start_octo_services.sh
+++ start_octo_services.sh
@@ -478,7 +478,7 @@ try:
     for agent in AGENTS:
         name = agent['name']
         test_msg = "【系統提示】測試發送訊息並報上名字"
-        agent_dir = os.path.join(os.environ['SCRIPT_DIR'], '..', 'agent_home', name)
+        agent_dir = os.path.join(os.environ['SCRIPT_DIR'], 'agent_home', name)
         flag_file = os.path.join(agent_dir, 'octo_cyberbrain', '.rotation_flag')
         pending_file = os.path.join(agent_dir, 'octo_cyberbrain', 'pending_inject.txt')
         
