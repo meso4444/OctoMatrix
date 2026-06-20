0860ce5b2fc1fac4709180e9ac878145ac9eab5d
meso4444 <tamotsuwang@gmail.com>
2026-06-20 14:50:52 +0800

    chore: apply Apache 2.0 License and meso4444 boilerplate headers

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2026 meso4444
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # install_docker_zh.sh - 智能 Docker & Docker Compose 安装腳本
 # 自動偵測環境（WSL/macOS/Linux）並應用最適當的安裝方法
 
